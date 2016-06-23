f4d04b8150d4b684d9f61372e13b592cbd9849ff
swissgerman <swiss.german69@gmail.com>
2016-06-23 14:25:35 +0200

    login und signup

--- RainbowMatchFrontend/src/rbm/loginView/loginView.component.js
+++ RainbowMatchFrontend/src/rbm/loginView/loginView.component.js
@@ -7,8 +7,18 @@
         };
 
         vm.login = function(){
+            //##################################
+            localStorage.userName = vm.user.userName;
+            jQuery.mobile.navigate("#home");
+            //##################################
+            
+            
             HttpService.post("http://localhost:20080/backend/users/login/",vm.user, function(event){
                 console.log(event);//10.4.57.190
+                if(event.id!=null){
+                    localStorage.userName = vm.user.userName;
+                    jQuery.mobile.navigate("#home");
+                }
             });
         };
     }
--- RainbowMatchFrontend/src/rbm/signupView/signupView.component.js
+++ RainbowMatchFrontend/src/rbm/signupView/signupView.component.js
@@ -14,14 +14,18 @@
             }
         }
         vm.signup = function(){
+            //#################################################
             if(repassword){
                 localStorage.username = vm.user.username;
                 window.location.href = "#home";
             }else{
                 alert("password not the same!");
             }
+            //##################################################
+            
+            
             if(repassword){
-            HttpService.post("10.4.57.190:20080/backend/users/user",vm.user,  function(event){
+                HttpService.post("10.4.57.190:20080/backend/users/user",vm.user,  function(event){
                 if(event.id==null){
                     localStorage.username = vm.user.username;
                     window.location.href = "#home";
@@ -30,8 +34,8 @@
                     alert("error");
                 }
                 
-            });
-                        }else{
+                });
+            }else{
                 alert("password not the same!");
             }
         }
