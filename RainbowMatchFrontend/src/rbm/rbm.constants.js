252b2e80c70adeb74e9def0287cb35f68fd91315
patrickgue <me@patrickguenthard.ch>
2016-06-23 22:10:51 +0200

    added js constants and images

---
+++ RainbowMatchFrontend/src/rbm/rbm.constants.js
@@ -0,0 +1,6 @@
+RainbowMatch = {
+    "host" : "http://localhost", //http://k-keller.com
+    "port" : "20080",
+    "beanName" : "/Backend/",
+    "baseUrl" : RainbowMatch.host + RainbowMatch.port + RainbowMatch.beanName
+}
