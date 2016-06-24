945486fd84abaf4aaa27e5a84e624d274a790c76
swissgerman <swiss.german69@gmail.com>
2016-06-24 09:33:16 +0200

    swipe

--- RainbowMatchFrontend/src/rbm/matchesView/matchesView.component.js
+++ RainbowMatchFrontend/src/rbm/matchesView/matchesView.component.js
@@ -17,7 +17,44 @@
                 }
             });
         };
+        document.addEventListener('touchstart', handleTouchStart, false);        
+        document.addEventListener('touchmove', handleTouchMove, false);
 
+        var xDown = null;                                                        
+        var yDown = null;                                                        
+
+        function handleTouchStart(evt) {                                         
+            xDown = evt.touches[0].clientX;                                      
+            yDown = evt.touches[0].clientY;                                      
+        };                                                
+
+        function handleTouchMove(evt) {
+            if ( ! xDown || ! yDown ) {
+                return;
+            }
+
+            var xUp = evt.touches[0].clientX;                                    
+            var yUp = evt.touches[0].clientY;
+
+            var xDiff = xDown - xUp;
+            var yDiff = yDown - yUp;
+
+            if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+                if ( xDiff > 0 ) {
+                    
+                    vm.nextMatch();
+                }                       
+            } else {
+                if ( yDiff > 0 ) {
+                    /* up swipe */ 
+                } else { 
+                    /* down swipe */
+                }                                                                 
+            }
+            /* reset values */
+            xDown = null;
+            yDown = null;                                             
+        };
         vm.nextMatch = function() {
           if(vm.currentMatch < vm.matches -1) {
             vm.currentMatch++;
