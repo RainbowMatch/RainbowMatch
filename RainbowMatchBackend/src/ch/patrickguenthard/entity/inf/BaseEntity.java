8101c632a9b62cac93c741defdc031818da6fb89
patrickgue <me@patrickguenthard.ch>
2016-09-01 21:38:53 +0200

    mod

--- RainbowMatchBackend/src/ch/patrickguenthard/entity/inf/BaseEntity.java
+++ RainbowMatchBackend/src/ch/patrickguenthard/entity/inf/BaseEntity.java
@@ -5,6 +5,13 @@ import java.sql.SQLException;
 
 import ch.patrickguenthard.exceptions.UnsupportedException;
 
+/**
+ * BaseEntity is an interface used in the persistance functionality of the application.
+ * It includes methods for creating INSERT strings and can create a object from a resultset.
+ * 
+ * @author Patrick
+ *
+ */
 public interface BaseEntity {
 	public String createPersistanceString() throws UnsupportedException;
 	public void createFromResultSet(ResultSet rs) throws SQLException;
--- RainbowMatchBackend/src/ch/patrickguenthard/entity/inf/DeletableEntity.java
+++ RainbowMatchBackend/src/ch/patrickguenthard/entity/inf/DeletableEntity.java
@@ -1,5 +1,10 @@
 package ch.patrickguenthard.entity.inf;
 
+/**
+ * adds possibility to create a delete string for a entity
+ * @author Patrick
+ *
+ */
 public interface DeletableEntity extends BaseEntity {
 	public String createDeleteString();
 }
